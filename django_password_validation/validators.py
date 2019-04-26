02df6cb2f0a73afb19db7d3f68412469fd03d98b
Luís Gomes <luis.gomes@fc.ul.pt>
2019-04-26 14:40:00 +0100

    Increased default minimum password length to 10

--- django_password_validation/validators.py
+++ django_password_validation/validators.py
@@ -25,7 +25,7 @@ DEFAULT_AUTH_PASSWORD_VALIDATORS = [
     }, {
         'NAME': 'django_password_validation.MinimumLengthValidator',
         'OPTIONS': {
-            'min_length': 9,
+            'min_length': 10,
         }
     }, {
         'NAME': 'django_password_validation.CommonPasswordValidator',
